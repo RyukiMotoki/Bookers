f353fb16ba6943871bb5b165659c4e8765a2da9f
RyukiMotoki <125866183+RyukiMotoki@users.noreply.github.com>
2023-03-14 14:30:47 +0000

    [Add] model、controller、レイアウトの実装

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,38 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(list.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to "/books"
+  end
+end
